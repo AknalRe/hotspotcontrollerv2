8dcaabd343cf347f055c2004f5c53f43e050142c
AknalRe <94711128+AknalRe@users.noreply.github.com>
2024-05-04 22:29:43 +0700

    Update

--- require/server.js
+++ require/server.js
@@ -38,7 +38,7 @@ app.use((req, res, next) => {
     const ips = forwardedFor.split(',').map(ip => ip.trim());
     req.ip = ips[0];
   } else {
-    req.ip = req.connection.remoteAddress;
+    next();
   }
   next();
 });
